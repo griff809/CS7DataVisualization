f290516e09817cd8cf904d3a162f2c4023e38d49
Griffin Steinman <steinmag@tcd.ie>
2024-10-14 13:29:13 +0100

    changes to a

--- Assignment1/assignment1partA.py
+++ Assignment1/assignment1partA.py
@@ -1,1 +1,20 @@
+#Choose a visual encoding channel to express GDP (gdpPercap) and life expectancy 
 
+#(lifeExp) respectively (e.g. x-position-position). Use the same channel for these 
+#attributes in all charts of Part A
+
+#Then create 3 variants of the charts that encode, the quantitative attribute 
+#population (pop) using 3 other encoding channels. 
+
+#✦ Ensure that Ireland is distinguishable from other countries (BUT it is not required 
+#that every single country is identifiable i.e., other data points can be anonymous)
+
+#✦ Each chart must created for viewing as a figure no larger than half an A4 page 
+
+#✦ It is optional to visualize the remaining attributes in this part.
+
+#✦ You are only required to visualize 2002 data for this part.
+
+#✦ In max half a page, add some brief text stating the tool(s) used for creating the 
+#visualizations. Discuss briefly your opinion on the efficacy of the visualizations in 
+#Charts #1 – 3.
